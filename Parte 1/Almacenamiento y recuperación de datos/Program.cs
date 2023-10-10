8e1413a6b6264fe208aa5f972689e212049f52a3
Leonel <leonelsanz15@gmail.com>
2023-10-10 16:07:03 -0300

    modulo 2

---
+++ Parte 1/Almacenamiento y recuperación de datos/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine('b');
+Console.WriteLine(123);
+Console.WriteLine(0.25F);
+Console.WriteLine(2.625);
+Console.WriteLine(12.39816m);
+
+Console.WriteLine(true);
+Console.WriteLine(false);
+
+string firstName;
+firstName = "Bob";
+Console.WriteLine(firstName);
+firstName = "Liem";
+Console.WriteLine(firstName);
+firstName = "Isabella";
+Console.WriteLine(firstName);
+firstName = "Yasmin";
+Console.WriteLine(firstName);
+
+string name = "Bob";
+int messages = 3;
+decimal temperature = 34.4m;
+
+Console.Write("Hello, ");
+Console.Write(name);
+Console.Write("! You have ");
+Console.Write(messages);
+Console.Write(" messages in your inbox. The temperature is ");
+Console.Write(temperature);
+Console.Write(" celsius.");
---
+++ Parte 1/Escritura de codigo C# por primera vez/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Hello World!");
+
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code.\n");
+
+Console.Write("This is the first");
+Console.WriteLine(" line.");
+Console.Write("This is the second");
+Console.WriteLine(" line.");
